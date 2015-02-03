7b61ed18589d8d86ee42eaf5612f0eea491a26e0
larissaleite <larissa.leite@gmail.com>
2015-02-02 21:04:27 -0300

    Simple REST API working

--- flask/bin/app.py
+++ flask/bin/app.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
-from flask import Flask, jsonify
-
-app = Flask(__name__)
+from flask import Flask, jsonify, abort, request, make_response, url_for, render_template
+ 
+app = Flask(__name__, static_url_path = "")
 
 tasks = [
     {
@@ -18,9 +18,43 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+ 
+@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify( { 'tasks': map(make_public_task, tasks) } )
+ 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    #if len(task) == 0:
+    #    abort(404)
+    return jsonify( { 'task': make_public_task(task[0]) } )
+ 
+@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+def create_task():
+    #if not request.json or not 'title' in request.json:
+    #    abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify( { 'tasks': map(make_public_task, tasks) } )
 
+#static files
+@app.route('/tasks', methods = ['GET'])
+def show_page():
+    return app.send_static_file('index.html')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
---
+++ flask/bin/static/js/controllers/task.js
@@ -0,0 +1,25 @@
+angular.module('todo', [])
+  .controller('TaskCtrl', ['$scope', '$http', function($scope, $http) {
+
+  	$http.get('/todo/api/v1.0/tasks')
+  		.success(function(response) {
+  			console.log(response);
+  			$scope.todolist = response.tasks;
+  		});  	
+
+  	$scope.register = function() {
+  		
+  		var task = {
+  			title : $scope._title,
+  		  description : $scope._description
+  		}
+
+  		$http.post('/todo/api/v1.0/tasks', task)
+  			.success(function(response) {
+  				console.log(response);
+          $scope.todolist = response.tasks;
+          console.log($scope.todolist.length);
+  			});
+  	}
+
+  }]);
